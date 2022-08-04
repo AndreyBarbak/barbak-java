28c6fa92cc32ac6d6ce44b83c9e1075b1212c1c7
Andrey_Barbak <abarbak@yandex.ru>
2022-08-04 13:08:46 +0200

    testTasks have been fixed

--- src/main/java/testTasks/Task1.java
+++ src/main/java/testTasks/Task1.java
@@ -1,14 +1,25 @@
 package testTasks;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите любое число");
-        int number = input.nextInt();
-        int controlNumber = 7;
-        String result = (number > controlNumber) ? "Привет" : "Попробуйте ещё";
-        System.out.println(result);
+        String inputString = input.nextLine();
+        inputString = inputString.replaceAll("\\s+", "").trim();//удаляем пробелы внутри и снаружи
+        if (inputString.matches("[0-9][0-9]*\\.[0-9][0-9]*|[0-9][0-9]*")) { //применяем регулярное выражение для отсечения букв и других символов
+            double number = Double.parseDouble(inputString);
+            double controlNumber = 7.0;
+            String result = (number > controlNumber) ? "Привет" : "Число меньше либо равно 7";
+            System.out.println(result);
+        } else {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("Вы ввели недопустимые символы");
+            }
+        }
     }
 }
--- src/main/java/testTasks/Task2.java
+++ src/main/java/testTasks/Task2.java
@@ -1,14 +1,29 @@
 package testTasks;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите имя");
-        String name = input.nextLine();
-        String expectedName = "Вячеслав";
-        String result = (name.equals(expectedName)) ? "Привет, Вячеслав" : "Нет такого имени";
-        System.out.println(result);
+        String inputName = input.nextLine();
+        String changeInputName = inputName.replaceAll("\\s+", "").trim();
+        if (changeInputName.matches("[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*[А-Я]*[а-я]*")) {
+
+            changeInputName = changeInputName.toLowerCase();
+
+            changeInputName = changeInputName.substring(0, 1).toUpperCase() + changeInputName.substring(1);
+
+            String expectedName = "Вячеслав";
+            String result = (changeInputName.equals(expectedName)) ? "Привет, Вячеслав" : "Нет такого имени";
+            System.out.println(result);
+        } else {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("Вы ввели недопустимые символы");
+            }
+        }
     }
 }
